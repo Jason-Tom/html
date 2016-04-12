3c5e924d704c6c029fae608229c1321240754813
Jason-Tom <429295712@qq.com>
2016-04-12 20:37:20 +0800

    有些属性无法指定

--- 响应式布局/js/myjs.js
+++ 响应式布局/js/myjs.js
@@ -5,19 +5,19 @@ function divtest() {
 }
 
 function chance() {
-    // var clientWidth = 0;
-    // document.getElementById("demo").innerHTML = document.body.clientWidth;
-    // document.getElementById("demo").innerHTML="My First JavaScript";
-    // clientWidth=document.body.clientWidth;
-    if (document.body.clientWidth < 300) {
-        document.getElementById("demo").className = "one";
-    } else if (document.body.clientWidth < 600) {
-        // document.getElementById("demo").className = "four";
-        document.getElementById("demo").style.backgroundColor="#6ffaff";
-        // document.getElementById("demo").style.color = "red";
-    } else {
-        document.getElementById("demo").className = "three";
-    }
-
+        // var clientWidth = 0;
+        // document.getElementById("demo").innerHTML = document.body.clientWidth;
+        // document.getElementById("demo").innerHTML="My First JavaScript";
+        // clientWidth=document.body.clientWidth;
+        if (document.body.clientWidth < 300) {
+            document.getElementById("demo").className = "one";
+            document.getElementById("demo").innerHTML = "one";
+        } else if (document.body.clientWidth < 600) {
+            document.getElementById("demo").className = "two";
+            document.getElementById("demo").innerHTML = "two";
+        } else {
+            document.getElementById("demo").className = "three";
+            document.getElementById("demo").innerHTML = "three";
+        }
 }
 window.onresize = chance;
