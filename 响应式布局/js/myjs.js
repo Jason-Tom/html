393f2f94c10475e3623f947bf17f7667c76cf6dd
Jason-Tom <429295712@qq.com>
2016-04-12 18:28:08 +0800

    通过onresize指定方法实现窗口启动时触发事件，不断重新调用函数

--- 响应式布局/js/myjs.js
+++ 响应式布局/js/myjs.js
@@ -4,15 +4,18 @@ function divtest() {
 }
 
 function chance() {
-    var clientWidth = 0;
-    document.getElementById("demo").innerHTML = document.body.clientWidth;
+    // var clientWidth = 0;
+    // document.getElementById("demo").innerHTML = document.body.clientWidth;
     // document.getElementById("demo").innerHTML="My First JavaScript";
-    clientWidth=document.body.clientWidth;
-    if (clientWidth < 300) {
+    // clientWidth=document.body.clientWidth;
+    if (document.body.clientWidth < 300) {
         document.getElementById("demo").className = "one";
-    } else if (clientWidth < 600) {
+        document.getElementById("demo").innerHTML="one";
+    } else if (document.body.clientWidth < 600) {
         document.getElementById("demo").className = "two";
+        document.getElementById("demo").innerHTML="two";
     } else {
         document.getElementById("demo").className = "three";
+        document.getElementById("demo").innerHTML="three";
     }
 }
