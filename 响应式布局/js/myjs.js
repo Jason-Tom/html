2e935b2092b595061315398edcab9ae817285f6d
Jason-Tom <429295712@qq.com>
2016-04-12 18:03:12 +0800

    不同尺寸窗口实现不同样式 但是不能自动化

--- 响应式布局/js/myjs.js
+++ 响应式布局/js/myjs.js
@@ -1,9 +1,18 @@
-function divtest(){
-	document.getElementById("test").className="two";
-	// alert("sdfsfsfs");
+function divtest() {
+    document.getElementById("test").className = "two";
+    // alert("sdfsfsfs");
 }
 
-function chance(){
-	document.getElementById("demo").innerHTML= document.body.clientWidth;
-	// document.getElementById("demo").innerHTML="My First JavaScript";
+function chance() {
+    var clientWidth = 0;
+    document.getElementById("demo").innerHTML = document.body.clientWidth;
+    // document.getElementById("demo").innerHTML="My First JavaScript";
+    clientWidth=document.body.clientWidth;
+    if (clientWidth < 300) {
+        document.getElementById("demo").className = "one";
+    } else if (clientWidth < 600) {
+        document.getElementById("demo").className = "two";
+    } else {
+        document.getElementById("demo").className = "three";
+    }
 }
